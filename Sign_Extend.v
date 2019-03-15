b61bbb433d210e3e6c349a21c9d632a1682633e3
milad hakimi <miladhakimi33@gmail.com>
2019-03-15 17:56:20 +0330

    bug fix

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -2,5 +2,5 @@ module Sign_Extend (
 		input [15:0] sign_input,
 		output [31:0] sign_output);
 		
-		assign sign_output = (sign_input[15] == 1)? {16'b11111_11111_11111_11111, sign_input}:{16'b0, sign_input};
+		assign sign_output = (sign_input[15] == 1)? {16'b1, sign_input}:{16'b0, sign_input};
 endmodule
